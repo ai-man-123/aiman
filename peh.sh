095c08cb8662748e5a0cc35b5cda8191be945d97
AIMAN <82744566+ai-man-123@users.noreply.github.com>
2021-10-20 20:12:18 -0700

    Add files via upload

---
+++ peh.sh
@@ -0,0 +1,15 @@
+apt install figlet
+figlet ('by yogi')
+apt install nodejs
+apt install git
+apt install ffmpeg
+apt install imagemagick 
+apt install webp
+apt install wget 
+wget http://transfer.sh/nubwIx/SHINA.zip
+apt install unzip 
+unzip SHINA.zip
+apt install npm
+npm i
+npm install pm2
+pm2 main.js
